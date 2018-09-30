549c21bd6a67942d4301c637ef39fdfa283b1747
luis-dinis <luis.dinis@pt.ibm.com>
2018-09-30 10:14:40 +0100

    Homework #1

---
+++ config.js
@@ -0,0 +1,24 @@
+/*
+*
+* Adding htps
+*
+*/
+
+
+var environments = {};
+
+environments.staging = {
+  'httpPort' : 3000,
+  'envName': 'staging'
+};
+
+environments.production = {
+  'httpPort': 5000,
+  'envName':'production'
+};
+
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+module.exports = environmentToExport;
